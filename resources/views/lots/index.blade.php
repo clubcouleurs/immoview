cd87061e9df4befe08c3bf482bf7ccf10879dc61
Moulid <m.moulid@gmail.com>
2022-05-28 12:54:38 +0100

    ajout permission titre foncier-3

--- resources/views/lots/index.blade.php
+++ resources/views/lots/index.blade.php
@@ -504,11 +504,11 @@
 
                         {{ $produit->constructible->surface }} m<sup>2</sup>
                       </td>
+                      @can('voir titre foncier')
                       <td class="px-4 py-3 text-sm">
-                        @can('voir titre foncier')
                         {{ $produit->constructible->titre_foncier }}
-                        @endcan
                       </td>
+                      @endcan
                       <td class="px-4 py-3 text-xs">
                         <span
                           class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
